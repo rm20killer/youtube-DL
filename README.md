ffe1296006144d27a19c03e0814298c044d91564
RM20 <43253883+rm20killer@users.noreply.github.com>
2023-08-25 14:33:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Youtube playlist downloader
+
+Quick and simple to download a whole playlist
+
+This script will also create a playlist info.txt which will show each song in the playlist and if it has been skipped or not.
+
+## How to download
+Click code and Download zip. 
+Unzp folder
+
+## how to use
+
+1) Install [node](https://nodejs.org/en) 
+2) Open folder with cmd
+3) Run the command `npm i` to install the packages needed
+4) Open config.json and replace URL with your own **playlist** link
+5) Run the command `node .\index.js`
+6) Wait for everything to download
---
+++ index.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const ytfps = require("ytfps");
+const config = require("./config.json");
+
+//loop through the playlist and download each video as a sprate mp3 file
+async function downloadPlaylist(playlist) {
+  console.log("Downloading playlist...");
+  console.log(playlist);
+  let videos = playlist.videos;
+  for (let i = 0; i < videos.length; i++) {
+    //make folder for playlist based on playlist title
+    if (!fs.existsSync(playlist.title)) {
+      fs.mkdirSync(playlist.title);
+    }
+    //create info file for playlist
+    if (!fs.existsSync(playlist.title + "/info.txt")) {
+        fs.writeFileSync(
+            playlist.title + "/info.txt",
+            "Playlist Title: " + playlist.title + "\n" +
+            "Playlist URL: " + playlist.url + "\n" +
+            "Playlist ID: " + playlist.id + "\n\n\n"
+        );
+        
+    }
+    //get info for video
+    let info = await ytdl.getInfo(videos[i].id);
+    let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+    console.log("Formats with only audio: " + audioFormats.length);
+    let audio = audioFormats[0];
+    console.log("Downloading: " + videos[i].title);
+    let name = videos[i].title.replace(/[^\w\s]/gi, "");
+    //if file already exists, skip
+    if (fs.existsSync(playlist.title + "/" + name + ".mp3")) {
+        console.log("File already exists, skipping...");
+        //add to info file
+        fs.appendFileSync(
+            playlist.title + "/info.txt",
+            name + " - " + videos[i].url + "- SKIPPED\n"
+        );
+        continue;
+    }
+    await ytdl(videos[i].id, { format: audio }).pipe(
+      fs.createWriteStream(playlist.title + "/" + name + ".mp3")
+    );
+    console.log("Downloaded: " + videos[i].title);
+    //add to info file
+    fs.appendFileSync(
+        playlist.title + "/info.txt",
+        name + " - " + videos[i].url + "\n"
+    );
+    console.log("Progress: " + ((i + 1) / videos.length) * 100 + "%");
+  }
+}
+
+async function ytlist() {
+  //Get url from config.json
+  const url = config.url;
+console.log(url);
+
+  //get id from url
+  const id = url.split("list=")[1].split("&")[0];
+  //get playlist
+  ytfps(id)
+    .then((playlist) => {
+      downloadPlaylist(playlist);
+    })
+    .catch((err) => {
+      handle_error(err);
+    });
+}
+
+ytlist(URL);
